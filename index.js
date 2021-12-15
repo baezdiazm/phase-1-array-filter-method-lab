0458a6c505d376ba0499cecf292547a38063e39d
baezdiazm <baezdiazm@yahoo.com>
2021-12-15 13:33:44 -0500

    commit tree fe15843a07cd2e21f2f01604d22465923057d8f4

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+function findMatching(collection, inquiry) {
+    return collection.filter(
+        (element) => element.toUpperCase() === inquiry.toUpperCase()
+        )
+    console.log(result)
+}
+    
+function fuzzyMatch(collection, provided) {
+    return collection.filter(
+        (element) => element.toUpperCase().indexOf(provided.toUpperCase()) === 0
+        )
+}
+
+function matchName(collection, provided) {
+    return collection.filter(
+        (element) => element.name === provided
+    )
+}
+        
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+fuzzyMatch(drivers, "b")
